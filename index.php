54673a72712832ef11195c931c1fe9e8408d4f06
root <emploi.neveu@gmail.com>
2018-11-27 11:11:03 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <form action="user.php" method="get">
+    <p>prénom<input type="text" name="prénom" value=""></p>
+    <p>nom<input type="text" name="nom" value=""></p>
+    <button type="submit" name="button">Valider</button>
+    </form>
+  </body>
+</html>
---
+++ user.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <?php
+    echo $_GET['prénom'].' '. $_GET['nom'];
+    ?>
+  </body>
+</html>
